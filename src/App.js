a086c1047d60a2e0e9b0735e0bb1401f3cf79947
unknown <Administrator@SILAB-44.ASIETDomain.com>
2020-02-12 15:49:16 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -4,23 +4,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    <div className="container">
+        <nav className="navbar navbar-light" style={{"backgroundColor": "#e3f2fd"}}>
+            <img src="p1.png" height="100" width="100"/><h1 style={{"color":"palevioletred"}}>AMIGOS CART</h1>
+            <button className="btn btn-primary">Home</button>
+          </nav>
+      <div className="row">
+        <div className="col-3">
+            <div className="card mb-3" style={{"maxWidth": "540px"}}>
+                <div className="row no-gutters">
+                  <div className="col-md-4">
+                    <img src="1.jpg" className="card-img" alt="img"/>
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h5 className="card-title">BOOKS</h5>
+                      <a href="#" className="btn btn-success">ADD</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        </div>
+        </div>
+      </div>
+  
+   );
 }
 
 export default App;
